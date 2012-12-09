efc358274fd7625cfd1f607ca68294e8620efd27
Yohan Boniface <yb@enix.org>
2012-12-10 00:29:20 +0100

    First version of an "update stats" control
    Just updating the bbox param and reloading the page for now.
    Need work in other branches to be useful

---
+++ reporter/static/js/reporter.js
@@ -0,0 +1,33 @@
+/* Update stats */
+L.Control.UpdateStats = L.Control.extend({
+
+    onAdd: function (map) {
+        var className = 'leaflet-control-stats',
+            container = L.DomUtil.create('div', className);
+
+        var link = L.DomUtil.create('a', "", container);
+        link.href = '#';
+        link.title = "Get stats for this view";
+        var fn = function (e) {
+            var bounds = map.getBounds(),
+                bbox = bounds.toBBoxString();
+            window.location = "?bbox=" + bbox;
+        };
+
+        L.DomEvent
+            .on(link, 'click', L.DomEvent.stopPropagation)
+            .on(link, 'click', L.DomEvent.preventDefault)
+            .on(link, 'click', fn, map)
+            .on(link, 'dblclick', L.DomEvent.stopPropagation);
+
+        return container;
+    }
+});
+
+L.Map.addInitHook(function () {
+    if (this.options.updateStatsControl) {
+        var options = this.options.statsControlOptions ? this.options.statsControlOptions : {};
+        this.updateStatsControl = new L.Control.UpdateStats(this, options);
+        this.addControl(this.updateStatsControl);
+    }
+});
