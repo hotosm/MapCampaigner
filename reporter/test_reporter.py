77fa0a3ab72b12405098fd4111686f68277aaead
Tim Sutton <tim@linfiniti.com>
2012-12-09 23:19:29 +0200

    Simple test class

--- reporter/test_reporter.py
+++ reporter/test_reporter.py
@@ -1,9 +1,27 @@
-from flask.ext.testing import TestCase
+# -*- coding: utf-8 -*-
+"""
+    Reporter Tests
 
-class MyTest(TestCase):
+    Tests the Flaskr application.
 
-    def create_app(self):
+    :copyright: (c) 2010 by Tim Sutton
+    :license: GPLv3, see LICENSE for more details.
+"""
+import reporter
+import unittest
 
-        app = Flask(__name__)
-        app.config['TESTING'] = True
-        return app
+
+class ReporterTestCase(unittest.TestCase):
+
+    def setUp(self):
+        reporter.app.config['TESTING'] = True
+        self.app = reporter.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_current_status(self):
+        return self.app.post('/', data=dict(), follow_redirects=True)
+
+if __name__ == '__main__':
+    unittest.main()
