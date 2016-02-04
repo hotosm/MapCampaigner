3cf6c3233157b0273a76d9ad676cb1e92a9ee5c0
Etienne Trimaille <etienne.trimaille@gmail.com>
2016-02-04 16:35:39 +0100

    update attribute table : name and address

--- reporter/resources/overpass/evacuation-centers/evacuation-centers.sql
+++ reporter/resources/overpass/evacuation-centers/evacuation-centers.sql
@@ -7,14 +7,14 @@ DELETE FROM planet_osm_point WHERE evacuation_center IS NULL;
 -- Merge planet_osm_point and planet_osm_polygon into one idp table
 DROP TABLE IF EXISTS evacuation_center;
 CREATE TABLE evacuation_center AS
-SELECT osm_id, evacuation_center, "name", "access:roof" AS access_roof, "addr:housename" AS house_name, "addr:housenumber" AS house_number,
+SELECT osm_id, evacuation_center, "name", "access:roof" AS access_roof, "addr:street" AS street, "addr:housename" AS house_name, "addr:housenumber" AS house_number,
 	amenity, building, "building:levels" as levels, "building:roof" as roof, "building:structure" as structure, "building:use" as building_use, "building:walls" as walls,
 	"capacity:persons" as capacity, "kitchen:facilities" as kitchen, leisure, office, "toilet:facilities" as toilet,
 	"toilets:number" as toilets_number, ref, religion, water_source,
 	ST_PointOnSurface(way) as way
 FROM planet_osm_polygon
 UNION
-SELECT osm_id, evacuation_center, "name", "access:roof" AS access_roof, "addr:housename" AS house_name, "addr:housenumber" AS house_number,
+SELECT osm_id, evacuation_center, "name", "access:roof" AS access_roof, "addr:street" AS street, "addr:housename" AS house_name, "addr:housenumber" AS house_number,
 	amenity, building, "building:levels" as levels, "building:roof" as roof, "building:structure" as structure, "building:use" as building_use, "building:walls" as walls,
 	"capacity:persons" as capacity, "kitchen:facilities" as kitchen, leisure, office, "toilet:facilities" as toilet,
 	"toilets:number" as toilets_number, ref, religion, water_source,
