fc4a14b55a8152ab1a85e6c00f2924ed35140fce
Dimas <dimas.ciputra@gmail.com>
2017-04-03 16:56:30 +0700

    Add date range input in frontend

--- reporter/static/js/reporter.js
+++ reporter/static/js/reporter.js
@@ -20,6 +20,11 @@ L.Control.UpdateStats = L.Control.extend({
             }
         }
 
+        L.DomEvent
+            .on(select, 'change', function (e) {
+                map.updateStatsControl.options.selected = e.target.value;
+            });
+
         var link = L.DomUtil.create('a', "", container);
         link.href = '#';
         link.innerHTML = "&nbsp;";
@@ -49,39 +54,39 @@ L.Map.addInitHook(function () {
 });
 
 $(function(){
-  $('.view-hm').click(function(e){
-    var username = $(this).attr("data-user");
-    $.ajax("/user", {
-      data: {username: username,
-             bbox: window.bbox},
-      success: function(data){
-        if (window.hmlayer) {
-          window.map.removeLayer(window.hmlayer);
-        }
-        var heatmap = new L.TileLayer.HeatCanvas({},{'step':0.07, 'degree':HeatCanvas.LINEAR, 'opacity':0.7});
-        $.each(data.d, function(i,e){
-          heatmap.pushData(e[0], e[1], 1);
-        })
-        window.map.addLayer(heatmap);
-        window.hmlayer = heatmap;
-      }
+    $('.view-hm').click(function(e){
+        var username = $(this).attr("data-user");
+        $.ajax("/user", {
+            data: {username: username,
+                bbox: window.bbox},
+            success: function(data){
+                if (window.hmlayer) {
+                    window.map.removeLayer(window.hmlayer);
+                }
+                var heatmap = new L.TileLayer.HeatCanvas({},{'step':0.07, 'degree':HeatCanvas.LINEAR, 'opacity':0.7});
+                $.each(data.d, function(i,e){
+                    heatmap.pushData(e[0], e[1], 1);
+                })
+                window.map.addLayer(heatmap);
+                window.hmlayer = heatmap;
+            }
+        });
     });
-  });
 
-  $('.clear-hm').click(function(e){
-    if (window.hmlayer) {
-      window.map.removeLayer(window.hmlayer);
-      delete window.hmlayer;
-    }
-  });
+    $('.clear-hm').click(function(e){
+        if (window.hmlayer) {
+            window.map.removeLayer(window.hmlayer);
+            delete window.hmlayer;
+        }
+    });
 
-  // Set up detail row hiding etc.
+    // Set up detail row hiding etc.
 
-  $('.details-toggle').click(function(e){
+    $('.details-toggle').click(function(e){
         var rowId = $(this).attr("value");
         $("#"+rowId).toggle();
-  });
-  $('#all-details').click(function(e){
+    });
+    $('#all-details').click(function(e){
         var status = $(this).attr("checked");
         if (status)
         {
@@ -92,6 +97,55 @@ $(function(){
             $(".details-row").hide();
         }
         $('.details-toggle').attr("checked", status);
-  });
+    });
+
+    var date_from = null;
+    var date_to = null;
+    var that = this;
+
+    $('#date-from').datepicker({
+        autoClose: true,
+        onSelect: function (fd, date) {
+            if (date) {
+                date_from = date.getTime();
+            }
+        }
+    });
+
+    $('#date-to').datepicker({
+        autoClose: true,
+        onSelect: function (fd, date) {
+            if (date) {
+                date_to = date.getTime();
+            }
+        }
+    });
+
+    $('#refresh-with-date').click(function () {
+        if(!date_from || !date_to) {
+            alert('Empty date');
+            return;
+        }
+
+        if(date_to < date_from) {
+            alert('Invalid range');
+            return;
+        }
+
+        var map = that.map || null;
+
+        if(!map) {
+            return;
+        }
+
+        var selected = map.updateStatsControl.options.selected;
+        var bounds = map.getBounds(),
+            bbox = bounds.toBBoxString();
+        window.location = "?bbox=" + bbox
+            + "&obj=" + selected
+            + "&date_from=" + date_from
+            + "&date_to=" + date_to;
+    });
+
 });
 
