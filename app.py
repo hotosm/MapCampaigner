4c6ceff079e2c704c6e160393723f163652dd2ee
Dimas <dimas.ciputra@gmail.com>
2017-05-15 11:50:14 +0700

    Add flask-admin

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from reporter import reporter
 from campaign_manager import campaign_manager
 import utilities
@@ -18,4 +20,11 @@ except KeyError:
 
 db = SQLAlchemy(osm_app)
 
+# Models
 from campaign_manager.models import OsmUser
+
+# Admin setting
+
+admin = Admin(osm_app, name='osm_reporter', template_mode='bootstrap3')
+
+admin.add_view(ModelView(OsmUser, db.session))
--- campaign_manager/models.py
+++ campaign_manager/models.py
@@ -6,9 +6,5 @@ class OsmUser(db.Model):
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
     def __repr__(self):
         return '<User %r>' % self.username
