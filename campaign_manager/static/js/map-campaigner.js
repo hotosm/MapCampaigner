50eb9aea7d5c02bc636185e22e8bc3c316b99e1a
Dimas <dimas.ciputra@gmail.com>
2017-05-23 11:34:25 +0700

    Disable manage campaign if user is not manager

--- campaign_manager/static/js/map-campaigner.js
+++ campaign_manager/static/js/map-campaigner.js
@@ -81,6 +81,18 @@ function clearNotification() {
     }
 }
 
+// UTILITIES
+
 function isEmpty(obj) {
     return Object.keys(obj).length === 0;
+}
+
+function containsObject(obj, list) {
+    var i;
+    for (i = 0; i < list.length; i++) {
+        if (list[i] === obj) {
+            return true;
+        }
+    }
+    return false;
 }
