8258168974550b72f39055319a5a92c05165dfdc
Dimas <dimas.ciputra@gmail.com>
2017-05-24 13:38:49 +0700

    Update insight panel size

--- campaign_manager/models/campaign.py
+++ campaign_manager/models/campaign.py
@@ -139,6 +139,7 @@ class Campaign(JsonModel):
         # render UI
         context = {
             'selected_function_name': selected_function.name().split('-')[0],
+            'icon': 'pie-chart',
             'widget': selected_function.get_ui_html()
         }
         campaing_ui += render_template(
