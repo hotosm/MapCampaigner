f27d56d732b086b54719f6e6bdc2ccde4c718354
meomancer <meomancer@gmail.com>
2017-05-25 14:49:39 +0700

    update completness insight function to show details and percentage

--- campaign_manager/selected_functions/__init__.py
+++ campaign_manager/selected_functions/__init__.py
@@ -3,5 +3,4 @@ __date__ = '17/05/17'
 
 from .count_feature import CountFeature
 from .feature_attribute_completeness import FeatureAttributeCompleteness
-from .feature_attribute_completeness_error import FeatureAttributeCompletenessError
 from .upload_shapefile import UploadShapefile
