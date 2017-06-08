1dedf16609051e7329eb649d1791a7bf7a3893e6
Anita Hapsari <anita@kartoza.com>
2017-06-08 10:07:53 +0700

    add osmcha error unittest
    * add osmcha error unittest
    
    * add mock data

--- flask_project/campaign_manager/test/helpers.py
+++ flask_project/campaign_manager/test/helpers.py
@@ -12,14 +12,24 @@ class CampaignObjectTest(Campaign):
         self.name = 'test'
         self.campaign_creator = 'anita'
         self.campaign_status = 'start'
-        self.coverage = {"last_uploader": "anita", "last_uploaded": "2017-06-06"}
-        self.geometry = None
+        self.coverage = {
+            "last_uploader": "anita", "last_uploaded": "2017-06-06"}
         self.start_date = None
         self.end_date = None
         self.campaign_managers = []
         self.selected_functions = []
         self.tags = []
         self.description = 'campaign object test'
+        self.geometry = {
+            "type": "FeatureCollection",
+            "features": [{"type": "Feature", "properties": {}, "geometry":
+                {"type": "Polygon", "coordinates":
+                    [[[20.430364608764652, -34.02157746626568],
+                      [20.439076423645023, -34.03064717674483],
+                      [20.45040607452393, -34.02506319517012],
+                      [20.45040607452393, -34.02349819173851],
+                      [20.434269905090336, -34.0185184433697],
+                      [20.430364608764652, -34.02157746626568]]]}}]}
 
     def get_coverage_folder(self):
         """ Return coverage folder for this campaign
---
+++ flask_project/campaign_manager/test/test_osmcha_error.py
@@ -0,0 +1,61 @@
+# coding=utf-8
+import unittest
+from unittest import mock
+from campaign_manager.insights_functions.osmcha_error import OsmchaError
+from campaign_manager.test.helpers import CampaignObjectTest
+
+class OsmchaErrorTestCase(unittest.TestCase):
+    """Test insight function to show osmcha error."""
+
+    def setUp(self):
+        """Constructor."""
+        self.campaign = CampaignObjectTest()
+        self.osmcha_error = OsmchaError(campaign=self.campaign)
+
+    def tearDown(self):
+        """Destructor."""
+        pass
+
+    def test_initiate(self):
+        self.osmcha_error.initiate({'int': '2'})
+        self.assertEquals(self.osmcha_error.current_page, 1)
+        self.osmcha_error.initiate({'page': '4'})
+        self.assertEquals(self.osmcha_error.current_page, 4)
+
+    def test_get_ui_html_file(self):
+        ui_html = self.osmcha_error.get_ui_html_file()
+        self.assertEquals(ui_html, 'osmcha_error')
+
+    def test_get_summary_html_file(self):
+        self.osmcha_error.get_summary_html_file = \
+            mock.MagicMock(return_value='html summary test')
+        summary_result = self.osmcha_error.get_summary_html_file
+        self.assertIsNotNone(summary_result)
+        self.assertEquals(summary_result.return_value, 'html summary test')
+
+    def test_details_html_file(self):
+        self.osmcha_error.get_details_html_file = \
+            mock.MagicMock(return_value='html details test')
+        details_result = self.osmcha_error.get_details_html_file
+        self.assertIsNotNone(details_result)
+        self.assertEquals(details_result.return_value, 'html details test')
+
+    def test_get_data_from_provider(self):
+        self.osmcha_error.get_data_from_provider = mock.MagicMock(
+            return_value={'max_page': '7', 'previous_page': '0',
+                          'current_page': '1', 'next_page': '2',
+                          'data': 'available'})
+        bbox_data = self.osmcha_error.get_data_from_provider()
+        self.assertIsNotNone(bbox_data)
+        self.assertEquals(bbox_data['max_page'], '7')
+        self.assertEquals(bbox_data['previous_page'], '0')
+        self.assertEquals(bbox_data['current_page'], '1')
+        self.assertEquals(bbox_data['next_page'], '2')
+        self.assertEquals(bbox_data['data'], 'available')
+
+    def test_process_data(self):
+        raw_data = self.osmcha_error.process_data(
+            raw_datas={'data1': '1234', 'data2': '2345'})
+        self.assertEquals(
+            raw_data, {'uuid': self.campaign.uuid,
+                       'data1': '1234', 'data2': '2345'})
