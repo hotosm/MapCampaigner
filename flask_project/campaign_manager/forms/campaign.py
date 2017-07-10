25caea0229fb005f6461400dd69aa9dd4899d965
Anita Hapsari <anita@kartoza.com>
2017-07-10 13:48:04 +0700

    update add or edit campaign ui (#152)
    * update add or edit campaign ui
    
    * add basic and advanced mode
    
    * Update css

--- flask_project/campaign_manager/forms/campaign.py
+++ flask_project/campaign_manager/forms/campaign.py
@@ -42,11 +42,13 @@ class CampaignForm(FlaskForm):
     name = StringField(
         u'Campaign name',
         validators=[DataRequired('Campaign name is needed')],
-        description='Name for the campaign'
+        description='Name for the campaign',
+        render_kw={'placeholder': 'Campaign name'}
     )
     description = TextAreaField(
         u'Campaign description',
-        description='Description for the campaign'
+        description='Description for the campaign',
+        render_kw={'placeholder': 'Campaign descriptions'}
     )
     campaign_status = RadioField(
         u'Campaign status',
@@ -57,9 +59,13 @@ class CampaignForm(FlaskForm):
         ]
     )
     start_date = DateField(
-            u'Start date of campaign')
+        u'Start date of campaign',
+        render_kw={'placeholder': '---'}
+    )
     end_date = DateField(
-            u'End date of campaign')
+        u'End date of campaign',
+        render_kw={'placeholder': '---'}
+    )
     campaign_managers = ManagerSelectMultipleField(
         u'Managers of campaign',
         validators=[DataRequired('Campaign manager is needed')])
@@ -76,7 +82,9 @@ class CampaignForm(FlaskForm):
     map_type = StringField(
         u'Campaign Map',
         description='Campaign manager may change the map view',
-        validators=[validate_map])
+        validators=[validate_map],
+        render_kw={'placeholder': 'Campaign map'}
+    )
     selected_functions = HiddenField(
         u'Selected Functions')
     submit = SubmitField(u'Submit')
