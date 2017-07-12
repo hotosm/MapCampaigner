682d41c59f70d350d0974e0616d075eda55778f2
Dimas Ciputra <dimas.ciputra@gmail.com>
2017-07-12 09:02:37 +0700

    Update feature completeness logic (#165)
    * Change how to check feature completeness
    
    * Warning lower/upper case for name
    
    * Tidy up
    
    * Fix count feature
    
    * Check operator
    
    * Ignore nodes

--- flask_project/campaign_manager/insights_functions/feature_attribute_completeness.py
+++ flask_project/campaign_manager/insights_functions/feature_attribute_completeness.py
@@ -11,6 +11,7 @@ class FeatureAttributeCompleteness(AbstractOverpassInsightFunction):
     need_required_attributes = True
     icon = 'list'
     _function_good_data = None  # cleaned data
+    nodes = {}
 
     def get_ui_html_file(self):
         """ Get ui name in templates
@@ -33,8 +34,107 @@ class FeatureAttributeCompleteness(AbstractOverpassInsightFunction):
         """
         return ""
 
+    def process_data(self, raw_datas):
+        """ Get geometry of campaign.
+        :param raw_datas: Raw data that returns by function provider
+        :type raw_datas: dict
+
+        :return: list good data
+        :rtype: dict
+        """
+        self._function_good_data = []
+        list_good_data = []
+        required_attributes = self.get_required_attributes()
+
+        if not raw_datas:
+            return []
+
+        for raw_data in raw_datas:
+
+            if raw_data['type'] == 'node':
+                continue
+
+            good_data = False
+
+            if 'tags' not in raw_data:
+                continue
+
+            raw_attr = raw_data["tags"]
+
+            for key, values in required_attributes.items():
+                tags_needed = raw_attr.get(key, None)
+
+                if tags_needed and tags_needed in [x.lower() for x in values]:
+                    good_data = True
+
+            if good_data:
+                # Check feature completeness
+                self.check_feature_completeness(raw_data)
+                self._function_good_data.append(raw_data)
+
+                if not raw_data['error']:
+                    list_good_data.append(raw_data)
+
+        return list_good_data
+
+    def check_feature_completeness(self, feature_data):
+        """Check feature completeness.
+
+        :param feature_data: Feature data
+        :type feature_data: dict
+        """
+        error_found = False
+        warning_message = ''
+        error_message = ''
+
+        # Check name tags
+        if 'name' not in feature_data['tags']:
+            error_found = True
+            error_message = 'Name not found'
+
+        # Check operator
+        if not error_found:
+            if 'operator:type' not in feature_data['tags'] \
+               and 'operator' not in feature_data['tags']:
+                error_found = True
+                error_message = 'Operator not found'
+
+        # Check all uppercase or lowercase
+        if not error_found:
+            feature_name = feature_data['tags']['name']
+            if feature_name.isupper():
+                error_found = True
+                warning_message = 'Name is all uppercase'
+            elif feature_name.islower():
+                error_found = True
+                warning_message = 'Name is all lowercase'
+
+        # Check mixed case
+        if not error_found:
+            feature_name = feature_data['tags']['name']
+            for index, name in enumerate(feature_name.split()):
+
+                if name[0].islower() and not self.is_string_int(name[0]):
+                    # e.g : name of Feature
+                    error_found = True
+                    warning_message = 'Name is mixed case'
+                    break
+
+        feature_data['error'] = error_found
+        feature_data['error_message'] = error_message
+        feature_data['warning_message'] = warning_message
+
+    def is_string_int(self, text):
+        """Check whether the text is int or not."""
+        try:
+            int(text)
+        except ValueError:
+            return False
+
+        return True
+
     def post_process_data(self, data):
-        """ Process data regarding output.
+        """Process data regarding output.
         This needed for processing data for counting or grouping.
 
         :param data: Data that received from open street map
