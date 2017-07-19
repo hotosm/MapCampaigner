5c1b85da74d6a27e617f24a836b770a20f860ef4
Anita Hapsari <anita@kartoza.com>
2017-07-19 09:22:22 +0700

    add remove tags (#197)
    * add delete tag button
    
    * add add-tag button
    
    * fix default view
    
    * fix UI bug on select step
    
    * update tags UI
    
    * update javascript to handle custom tags
    
    * fix remove/selection tags not working in firefox
    
    * fix tags on edit page

--- flask_project/campaign_manager/static/js/map-campaigner.js
+++ flask_project/campaign_manager/static/js/map-campaigner.js
@@ -2,14 +2,14 @@
 window.isAuthenticated = false;
 
 
-$(function() {
+$(function () {
     $('#side-menu').metisMenu();
 });
 
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
-$(function() {
+$(function () {
 
     var topOffset = 50;
     var height = (($(window).innerHeight > 0) ? $(window).innerHeight : $(window).height()) - 1;
@@ -19,7 +19,7 @@ $(function() {
         $("#page-wrapper").css("min-height", (height) + "px");
     }
 
-    $(window).bind("load resize", function() {
+    $(window).bind("load resize", function () {
         var topOffset = 50;
         var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
@@ -41,7 +41,7 @@ $(function() {
     // var element = $('ul.nav a').filter(function() {
     //     return this.href == url;
     // }).addClass('active').parent().parent().addClass('in').parent();
-    var element = $('ul.nav a').filter(function() {
+    var element = $('ul.nav a').filter(function () {
         return this.href == url;
     }).addClass('active').parent();
 
@@ -57,7 +57,7 @@ $(function() {
 
 function showLoading() {
     $.LoadingOverlay("show",
-        {image : "static/resources/loading-spinner.gif"});
+        {image: "static/resources/loading-spinner.gif"});
 }
 
 function hideLoading() {
@@ -68,15 +68,15 @@ function showNotifications(text, status) {
     clearNotification();
 
     $('#first-row').prepend(
-        '<div id="notification" class="alert alert-'+status+' alert-dismissable" style="margin-bottom: 10px; margin-top: 0;">' +
-            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>'+
-            text+
+        '<div id="notification" class="alert alert-' + status + ' alert-dismissable" style="margin-bottom: 10px; margin-top: 0;">' +
+        '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>' +
+        text +
         '</div>'
     );
 }
 
 function clearNotification() {
-    if($('#notification').length > 0) {
+    if ($('#notification').length > 0) {
         $('#notification').remove();
     }
 }
@@ -104,7 +104,7 @@ function capitalizeFirstLetter(string) {
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
-    for (var i = 0; i < 6; i++ ) {
+    for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
@@ -113,12 +113,12 @@ function getRandomColor() {
 function hashCode(str) { // java String#hashCode
     var hash = 0;
     for (var i = 0; i < str.length; i++) {
-       hash = str.charCodeAt(i) + ((hash << 5) - hash);
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
     }
     return hash;
 }
 
-function intToRGB(i){
+function intToRGB(i) {
     var c = (i & 0x00FFFFFF)
         .toString(16)
         .toUpperCase();
@@ -126,3 +126,10 @@ function intToRGB(i){
     return "00000".substring(0, 6 - c.length) + c;
 }
 
+function check(array1, array2) {
+    for (var i = 1; i < array1.length; i++) {
+        $.inArray(5 + 5, ["8", "9", "10", 10 + ""]);
+    }
+
+    return true;
+}
