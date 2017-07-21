65dac26d81d5c30d520dcfefd574bcda66b91063
Anita Hapsari <anita@kartoza.com>
2017-07-21 09:37:58 +0700

    add current user as default manager when manager is empty (#213)
    * add current user as default manager when manager is empty
    
    * remove x-button for default manager also fixed next and prev buttons
    
    * fix button

--- flask_project/campaign_manager/views.py
+++ flask_project/campaign_manager/views.py
@@ -645,6 +645,7 @@ def edit_campaign(uuid):
     context['maximum_area_size'] = MAX_AREA_SIZE
     context['uuid'] = uuid
     context['types'] = {}
+    context['campaign_creator'] = campaign.campaign_creator
     try:
         context['types'] = json.dumps(
             get_types()).replace('True', 'true').replace('False', 'false')
