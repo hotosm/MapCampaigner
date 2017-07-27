88625484966718b688c20b0d26fae9074dfa4534
Dimas <dimas.ciputra@gmail.com>
2017-07-27 07:31:16 +0700

    Fix mapper engagement chart

--- flask_project/reporter/test/test_utilities.py
+++ flask_project/reporter/test/test_utilities.py
@@ -42,23 +42,6 @@ class UtilitiesTestCase(LoggedTestCase):
         with self.assertRaises(ValueError):
             split_bbox('invalid bbox string')
 
-    def test_osm_building_contributions(self):
-        """Test that we can obtain correct contribution counts for a file."""
-        file_handle = open(FIXTURE_PATH)
-        contributor_list = osm_object_contributions(
-            file_handle,
-            tag_name="building")
-        expected_list = ast.literal_eval(open(os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),
-            'test_data',
-            'expected_osm_building_contributions.txt'
-        ), 'rb').read().decode('utf-8'))
-
-        # noinspection PyPep8Naming
-        self.maxDiff = None
-        print(contributor_list)
-        self.assertListEqual(contributor_list, expected_list)
-
     def test_get_totals(self):
         """Test we get the proper totals from a sorted user list."""
         sorted_user_list = osm_object_contributions(
