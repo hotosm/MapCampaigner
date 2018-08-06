f1b99e2e9e61fb6ad715f1f63afb030d5c2513a1
pierrealixt <pierrealix@kartoza.com>
2018-08-06 10:20:38 +0200

    lambda download functions

---
+++ lambda_functions/download/attic_data/parser.py
@@ -0,0 +1,35 @@
+import datetime
+import xml.sax
+import os
+import json
+
+
+class ElementParser(xml.sax.ContentHandler):
+
+    def __init__(self):
+        xml.sax.ContentHandler.__init__(self)
+        self.elements_ids = {
+            'node': [],
+            'way': [],
+            'relation': []
+        }
+    def startDocument(self):
+        return
+
+    def endDocument(self):
+        return
+
+    def startElement(self, name, attrs):
+        if name == 'node':
+            element_id = attrs.getValue('id')
+            self.elements_ids['node'].append(str(element_id))
+        elif name == 'way':
+            element_id = attrs.getValue('id')
+            self.elements_ids['way'].append(str(element_id))
+        elif name == 'relation':
+            element_id = attrs.getValue('id')
+            self.elements_ids['relation'].append(str(element_id))
+
+
+    def endElement(self, name):
+        return
---
+++ lambda_functions/download/features/test_helper.py
@@ -0,0 +1,35 @@
+SELECTED_FUNCTIONS = {
+    "function-1": {
+        "function": "FeatureAttributeCompleteness",
+        "feature": "amenity=cafe", "attributes": {
+            "amenity": ["cafe"], "name": [], "cuisine": [], "operator": [],
+            "opening_hours": []},
+        "type": "cafe"},
+    "function-2": {
+        "function": "CountFeature",
+        "feature": "amenity=cafe", "attributes": {
+            "amenity": ["cafe"], "name": [], "cuisine": [], "operator": [],
+            "opening_hours": []},
+        "type": "cafe"},
+    "function-3": {
+        "function": "MapperEngagement",
+        "feature": "amenity=cafe", "attributes": {
+            "amenity": ["cafe"], "name": [], "cuisine": [], "operator": [],
+            "opening_hours": []},
+        "type": "cafe"},
+    "function-4": {
+        "function": "FeatureAttributeCompleteness",
+        "feature": "shop=supermarket", "attributes": {
+            "shop": ["supermarket"], "name": []},
+        "type": "supermarket"},
+    "function-5": {
+        "function": "CountFeature",
+        "feature": "shop=supermarket", "attributes": {
+            "shop": ["supermarket"], "name": []},
+        "type": "supermarket"},
+    "function-6": {
+        "function": "MapperEngagement",
+        "feature": "shop=supermarket", "attributes": {
+            "shop": ["supermarket"], "name": []},
+        "type": "supermarket"}
+}
