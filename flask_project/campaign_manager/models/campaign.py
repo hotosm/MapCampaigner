c6ea89de47afb4c8b47fef15cc0de590982f9770
pierrealixt <pierrealix@kartoza.com>
2018-09-05 13:44:11 +0200

    generate thumbnail after campaign creation

--- flask_project/campaign_manager/models/campaign.py
+++ flask_project/campaign_manager/models/campaign.py
@@ -534,6 +534,9 @@ class Campaign(JsonModel):
         geocampaign_body = json.dumps(parse_json_string(geometry))
         S3Data().create(geocampaign_key, geocampaign_body)
 
+        campaign = Campaign(data['uuid'])
+        campaign.generate_static_map()
+
     @staticmethod
     def all(campaign_status=None, **kwargs):
         """Get all campaigns
