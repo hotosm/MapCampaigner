8925ea92fb7ff2b6c65298f545f1d1f6f60492fd
pierrealixt <35932320+pierrealixt@users.noreply.github.com>
2019-02-13 17:45:18 +0200

    Fixed the failed thumbnail test. (#639)

--- flask_project/campaign_manager/test/test_thumbnail.py
+++ flask_project/campaign_manager/test/test_thumbnail.py
@@ -6,23 +6,10 @@ import requests
 from campaign_manager.models.campaign import Campaign
 
 
-# get the MAPBOX_TOKEN from AWS Beanstalk settings.
-MAPBOX_TOKEN = ''
-
-
-# cd flask_project/campaign_manager/test
-# nosetests campaign_manager.test.test_thumbnail
 class TestGenerateThumbnail(TestCase):
-    def setUp(self):
-        # set APP_SETTINGS to 'app_config.AWSProductionConfig'
-        # if you want to fetch a campaign in production
-        os.environ['APP_SETTINGS'] = 'app_config.AWSStagingConfig'
-        os.environ['MAPBOX_TOKEN'] = MAPBOX_TOKEN
-
     def runTest(self):
-        campaign = Campaign('')
+        campaign = Campaign('ff6ff8fcfdd847c48dd1bc3c9107b397')
         url = campaign.generate_static_map_url(simplify=False)
-        print(url)
 
         request = requests.get(url, stream=True)
         self.assertEqual(request.status_code, 200)
