7b5729ee49826728a05c8084b7065e797289218c
Jo Sprague <josiah.sprague@gmail.com>
2019-10-31 01:16:25 +0100

    Enable hot reloading for css
    This doesn't work well with script-loader so there are some errors in
    the browser console, and vendor scripts might not load but it works.

--- webpack.config.js
+++ webpack.config.js
@@ -5,44 +5,11 @@ const mode = process.env.NODE_ENV;
 
 module.exports = [
   {
-    entry: {
-      // These scripts are installed from package.json with npm
-      vendor: [
-        'jquery/dist/jquery.min.js',
-        'bootstrap/dist/js/bootstrap.min.js', // TODO: Only include needed comps
-        'metismenu/dist/metisMenu.min.js',
-        'leaflet/dist/leaflet.js',
-        'mapbox-gl/dist/mapbox-gl.js',
-        'gasparesganga-jquery-loading-overlay/src/loadingoverlay.min.js',
-        'datatables.net/js/jquery.dataTables.min.js',
-        'datatables.net-rowreorder/js/dataTables.rowReorder.min.js',
-        'datatables.net-responsive/js/dataTables.responsive.min.js',
-        'moment/moment.js',
-        'datatables.net-plugins/sorting/datetime-moment.js',
-        'chart.js/dist/Chart.bundle.min.js'
-      ],
-      // These scripts are committed to this repo
-      legacy: [
-        './flask_project/campaign_manager/static/js/map-campaigner.js',
-        './flask_project/campaign_manager/static/libs/geojson-bounds/geojson-bounds.js',
-        './flask_project/campaign_manager/static/libs/osmauth.js',
-        './flask_project/campaign_manager/static/libs/leaflet-color-markers/leaflet-color-markers.js',
-        './flask_project/campaign_manager/static/js/datepicker.min.js',
-        './flask_project/campaign_manager/static/js/datepicker.en.js',
-        './flask_project/campaign_manager/static/libs/leaflet-search.js',
-        './flask_project/campaign_manager/static/libs/js.cookie-2.1.4.min.js',
-        './flask_project/campaign_manager/static/libs/jquery-file-upload/jquery.ui.widget.js',
-        './flask_project/campaign_manager/static/libs/jquery-file-upload/jquery.fileupload.js'
-      ]
+    devServer: {
+      watchContentBase: true,
+      writeToDisk: true,
+      proxy: {'/': 'http://0.0.0.0:5000'}
     },
-    output: {
-      filename: '[name].js',
-      path: path.resolve(__dirname, 'flask_project/campaign_manager/static/dist'),
-    },
-    module: { rules: [{ test: /\.js$/, use: 'script-loader' }] },
-    mode
-  },
-  {
     entry: {
       main: [
         './flask_project/campaign_manager/index.js',
@@ -96,6 +63,44 @@ module.exports = [
         }
       ],
     },
+    mode
+  },
+  {
+    entry: {
+      // These scripts are installed from package.json with npm
+      vendor: [
+        'jquery/dist/jquery.min.js',
+        'bootstrap/dist/js/bootstrap.min.js', // TODO: Only include needed comps
+        'metismenu/dist/metisMenu.min.js',
+        'leaflet/dist/leaflet.js',
+        'mapbox-gl/dist/mapbox-gl.js',
+        'gasparesganga-jquery-loading-overlay/src/loadingoverlay.min.js',
+        'datatables.net/js/jquery.dataTables.min.js',
+        'datatables.net-rowreorder/js/dataTables.rowReorder.min.js',
+        'datatables.net-responsive/js/dataTables.responsive.min.js',
+        'moment/moment.js',
+        'datatables.net-plugins/sorting/datetime-moment.js',
+        'chart.js/dist/Chart.bundle.min.js'
+      ],
+      // These scripts are committed to this repo
+      legacy: [
+        './flask_project/campaign_manager/static/js/map-campaigner.js',
+        './flask_project/campaign_manager/static/libs/geojson-bounds/geojson-bounds.js',
+        './flask_project/campaign_manager/static/libs/osmauth.js',
+        './flask_project/campaign_manager/static/libs/leaflet-color-markers/leaflet-color-markers.js',
+        './flask_project/campaign_manager/static/js/datepicker.min.js',
+        './flask_project/campaign_manager/static/js/datepicker.en.js',
+        './flask_project/campaign_manager/static/libs/leaflet-search.js',
+        './flask_project/campaign_manager/static/libs/js.cookie-2.1.4.min.js',
+        './flask_project/campaign_manager/static/libs/jquery-file-upload/jquery.ui.widget.js',
+        './flask_project/campaign_manager/static/libs/jquery-file-upload/jquery.fileupload.js'
+      ]
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'flask_project/campaign_manager/static/dist'),
+    },
+    module: { rules: [{ test: /\.js$/, use: 'script-loader' }] },
     mode
   }
 ];
