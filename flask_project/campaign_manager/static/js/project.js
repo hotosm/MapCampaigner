5eea28e5f1c371cd93cd6e4ac5b4e04a2fe3f5f8
Éléonore Mayola <eleonore.mayola@gmail.com>
2019-11-11 17:26:32 +0100

    WIP- leaving index.js alone to fix CI checks, woring on popup

--- flask_project/campaign_manager/static/js/project.js
+++ flask_project/campaign_manager/static/js/project.js
@@ -1,15 +1,20 @@
 function deleteRequest() {
+  // alert("Foo");
   var popup = document.getElementById("delete-pop");
   var background = document.getElementById("delete-background");
   var backgrdHeight = document.documentElement.clientHeight;
+  // console.log(backgrdHeight); //462
   var windowWidth = document.body.parentNode.clientWidth;
+  // console.log(windowWidth); // 1847
   if (popup.style.display == "none" && background.style.display == "none") {
     popup.style.display = "block";
-    popup.style.top = backgrdHeight/2 - 200 + "px";
-    popup.style.left = windowWidth/2 - 200 + "px";
-    popup.style.background-color = rgba(147, 146, 146, 0.7);
+    // popup.style.top = backgrdHeight/2 - 100 + "px";
+    // console.log(popup.style.top);
+    // popup.style.left = windowWidth/2 - 200 + "px";
+    // console.log(popup.style.left);
+    // popup.style.background-color = rgba(147, 146, 146, 0.7);
     background.style.display = "block";
-    background.style.height = backgrdHeight + "px";
+    // background.style.height = backgrdHeight + "px";
   } else {
     popup.style.display = "none";
     background.style.display = "none";
