5169fa07faefc97d9060a3f83dbfa168bf5a4590
Éléonore Mayola <eleonore.mayola@gmail.com>
2019-11-12 16:46:55 +0100

    WIP- checkbox style and function

--- flask_project/campaign_manager/static/js/project.js
+++ flask_project/campaign_manager/static/js/project.js
@@ -1,11 +1,20 @@
 function deleteRequest() {
-  var popup = document.getElementById("delete-pop");
-  var background = document.getElementById("delete-background");
+  const popup = document.getElementById("delete-pop");
+  const background = document.getElementById("delete-background");
   if (popup.style.display == "none" && background.style.display == "none") {
     popup.style.display = "inline-table";
     background.style.display = "block";
   } else {
     popup.style.display = "none";
     background.style.display = "none";
+  }
+}
+
+function confirmDelete() {
+  const deleteBtn = document.getElementById("delete-btn");
+  if (deleteBtn.hasAttribute("disabled")){
+    deleteBtn.removeAttribute("disabled");
+  } else {
+   deleteBtn.setAttribute("disabled", " ");
   }
 }
