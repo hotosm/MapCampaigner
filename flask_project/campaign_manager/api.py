86a54345aa09d677089fdd9419227f819ae44d4a
Russ Biggs <russbiggs@gmail.com>
2019-12-19 18:09:53 -0700

    updated api endpoints

--- flask_project/campaign_manager/api.py
+++ flask_project/campaign_manager/api.py
@@ -11,6 +11,7 @@ import boto3
 import json
 from zipfile import ZipFile
 import xml.etree.ElementTree as xee
+from xml.etree.ElementTree import fromstring, ElementTree, XMLParser, tostring
 
 from campaign_manager import campaign_manager
 from campaign_manager.models.campaign import Campaign
@@ -317,6 +318,7 @@ def filter_json(json_data, filters):
         json_data = [item for item in json_data if item[k] == v]
     return json_data
 
+
 def filter_xml(xml_data, filters):
     if not filters:
         return xml_data
@@ -331,28 +333,54 @@ def filter_xml(xml_data, filters):
     return xee.tostring(doc)
 
 
+def merge_xml(xml_files):
+    node = None
+    parser = XMLParser(encoding="utf-8")
+    for xml_file in xml_files:
+        tree = ElementTree(fromstring(xml_file))
+        root = tree.getroot()
+        if node is None:
+            node = root
+        else:          
+            for child in root:
+                if child.tag in ('way', 'node'):
+                    node.append(child)
+    return tostring(node)
+
+
 class DownloadFeatures(Resource):
     """ download feature list """
 
     def post(self, uuid):
         parser = reqparse.RequestParser()
         parser.add_argument('fileFormat', type=str)
-        parser.add_argument('featureName', type=str)
+        parser.add_argument('username', type=str)
         parser.add_argument('filter', type=dict)
         args = parser.parse_args()
         file_format = args.get('fileFormat', None)
-        feature_name = args.get('featureName', None)
+        username = args.get('username', None)
         filters = args.get('filter', {})
+        campaign = S3Data().fetch(f'campaigns/{uuid}/campaign.json')
+        features = [f['type'].replace(' ', '_') for _, f
+                in campaign['types'].items()]
         if file_format == "csv":
             file_buffer = StringIO()
-            data = S3Data().fetch(f'campaigns/{uuid}/{feature_name}.json')
+            data = []
+            for feature in features:
+                feature_json = S3Data().fetch(f'campaigns/{uuid}/{feature}.json')
+                data += feature_json
+            if username:
+                data = [item for item in data if item['last_edited_by'] == username]
             headers = list(data[0].keys())
+            for row in data:
+                for item in row['attributes']:
+                    if item not in headers:
+                        headers.append(item)
+                for item in row['missing_attributes']:
+                    if item not in headers:
+                        headers.append(item)
             data = filter_json(data, filters)
             if len(data) > 0:
-                for item in data[0]['attributes']:
-                    headers.append(item)
-                for item in data[0]['missing_attributes']:
-                    headers.append(item)
                 for row in data:
                     for item in row['attributes']:
                         row[item] = 1
@@ -360,10 +388,15 @@ class DownloadFeatures(Resource):
                         row[item] = 0
             headers.remove('missing_attributes')
             headers.remove('attributes')
+            for col in headers:
+                for row in data:
+                    if col not in row:
+                        row[col] = ''
             for d in data:
                 del d['attributes']
             for d in data:
                 del d['missing_attributes']
+            return data
             writer = csv.DictWriter(file_buffer,fieldnames=headers)
             writer.writeheader()
             writer.writerows(data)
@@ -376,20 +409,27 @@ class DownloadFeatures(Resource):
             file_buffer = BytesIO()
             mimetype = 'text/xml'
             s3 = S3Data().s3
-            key = f'campaigns/{uuid}/overpass/{feature_name}.xml'
-            data = s3.get_object(Bucket=S3Data().bucket, Key=key)['Body'].read()
-            data = filter_xml(data, filters)
+            xmls = []
+            for feature in features:
+                key = f'campaigns/{uuid}/overpass/{feature}.xml'
+                data = s3.get_object(Bucket=S3Data().bucket, Key=key)['Body'].read()
+                data = filter_xml(data, filters)
+                xmls.append(data)
+            data = merge_xml(xmls)
             file_buffer.write(data)
             file_buffer.seek(0)
             response_file = file_buffer
         if file_format == None:
             return
+        feature_name = "all_features"
+        if username:
+            feature_name = username
+
         resp = send_file(response_file,
                     as_attachment=True,
                     attachment_filename=f'{feature_name}.{file_format}',
                     mimetype=mimetype)
         return resp
-
 
 
 class DownloadFeature(Resource):
